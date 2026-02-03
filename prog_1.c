04000eeefc3a1c8e731da1abd369f97bb8d2288b
joyur98 <kcruyoj7@gmail.com>
2026-02-03 22:54:45 +0545

    addes push and pop operations

--- prog_1.c
+++ prog_1.c
@@ -6,6 +6,25 @@
 char stack[100]; //creating a stack of 100 size
 int top = -1; //for now, the stack is empty
 
+void push(char c){
+    if(top < 99){ //checks if stack has space
+        stack[++top] = c;
+    }
+    else{
+        printf("Stack Overflow"); //prints when stack has no space
+    }
+}
+
+char pop(){
+    if(top >= 0){ //checks if stack is empty
+        return stack[top--];
+    }
+    else{
+        printf("Stack is empty"); //prints when stack is empty
+        return '\0'; //returning something for safetl
+    }
+}
+
 int main(){
 
     return 0;
