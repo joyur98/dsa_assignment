65b28ba8220b3e5a2ef2011c9fc7df523800eeb8
joyur98 <kcruyoj7@gmail.com>
2026-02-03 22:44:28 +0545

    made the prog_1 file and made a stack in it

---
+++ prog_1.c
@@ -0,0 +1,12 @@
+// to check if an expression has balanced number of parantheses or not
+
+#include <stdio.h>
+#include <string.h>
+
+char stack[100]; //creating a stack of 100 size
+int top = -1; //for now, the stack is empty
+
+int main(){
+
+    return 0;
+}
