4898fcc519f778f7716372272e66b4e0c4333d6d
joyur98 <kcruyoj7@gmail.com>
2026-02-07 15:56:56 +0545

    made the main function and the code works with the given example

--- prog_5.c
+++ prog_5.c
@@ -44,12 +44,12 @@ void BFS(int start, int n){
 
 //DFS function
 void DFS(int start, int n){
-    printf("%d", start);
-    visited[start] = 1;
+    printf("%d", start); //prints the current node that is start
+    visited[start] = 1; //marks the node as start so as to not print it twice
 
     for (int j = 0; j < n ; j++){
         if(adj[start][j] == 1 && visited[j] == 0){
-            DSF(j, n);
+            DFS(j, n); //calls itself if neighbor exists but is unvisited
         }
     }
 }
@@ -61,7 +61,33 @@ void DFS_Start(int start, int n){
         visited[i] = 0; //marking the nodes as unvisited
     }
 
-    print("DFS Traversal: ");
-    DFS(start,n);
+    printf("DFS Traversal: ");
+    DFS(start,n); // starts the DFS function
     printf("\n");
+}
+
+
+int main(){
+    int n = 5; //number of vertices
+
+    addEdge(0, 1); // it makes it so that there is an edge between 0 and 1 now
+    addEdge(0, 2);
+    addEdge(0, 4);
+    addEdge(1, 2);
+    addEdge(2, 3);
+    addEdge(2, 4);
+
+    printf("Adjacency matrix: \n");
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            printf("%d", adj[i][j]);
+        }
+        printf("\n");
+    }
+
+    BFS(0, n);
+    DFS_Start(0, n);
+
+    return 0;
 }
